7b2129bb71ec23e1e19bec4b13e7cf1ead077826
Jimmie Fulton <jimmie.fulton@naxgrp.com>
2023-10-21 02:27:53 -0700

    initial commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+-- Nicer filetree than NetRW
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		config = function()
+			require("neo-tree").setup{
+        close_if_last_window = true,
+      }
+			local map = require("utils.keys").map
+			map( { "n", "v" }, "<leader>fe", "<cmd>Neotree close<cr><cmd>Neotree focus float toggle reveal<cr>", "Explore in Float")
+			map( { "n", "v" }, "<leader>fE", "<cmd>Neotree focus left toggle reveal<cr>", "Explore in Bar")
+			map( { "n", "v" }, "<leader>fr", "<cmd>Neotree focus left reveal<cr>", "Reveal in Bar")
+			map( { "n", "v" }, "<leader>be", "<cmd>Neotree buffers float focus reveal<cr>", "Reveal in Float")
+		end,
+	},
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+-- Telescope fuzzy finding (all the things)
+return {
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = vim.fn.executable("make") == 1 },
+		},
+		config = function()
+      local actions = require "telescope.actions"
+
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							--["<C-u>"] = false,
+							--["<C-d>"] = false,
+						},
+            n = {
+              ["q"] = actions.close,
+            },
+					},
+				},
+			})
+
+			-- Enable telescope fzf native, if installed
+			pcall(require("telescope").load_extension, "fzf")
+
+			local map = require("utils.keys").map
+--			map("n", "<leader>fr", require("telescope.builtin").oldfiles, "Recently opened")
+			map("n", "<leader><space>", require("telescope.builtin").buffers, "Open buffers")
+			map("n", "<leader>bb", require("telescope.builtin").buffers, "Find")
+			map("n", "<leader>/", require("telescope.builtin").current_buffer_fuzzy_find, "Search File")
+			map("n", "<leader>ff", require("telescope.builtin").find_files, "Find")
+			map("n", "<leader>sh", require("telescope.builtin").help_tags, "Help")
+			map("n", "<leader>sw", require("telescope.builtin").grep_string, "Current word")
+			map("n", "<leader>fg", require("telescope.builtin").live_grep, "Grep")
+			map("n", "<leader>sd", require("telescope.builtin").diagnostics, "Diagnostics")
+
+			map("n", "<C-p>", require("telescope.builtin").keymaps, "Search keymaps")
+		end,
+	},
+}
